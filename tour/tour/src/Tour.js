c30ef4e5013418e2264e7a0e3dd0352f690b04a0
hasset <hassetwendesen653@gmail.com>
2025-08-05 17:19:01 +0300

    componets of the tour

--- tour/tour/src/Tour.js
+++ tour/tour/src/Tour.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Tour = (tour) => {
+  const [readmore,setreadmore]=useState(false)
   return (
     <article className='single-tour'>
       <img src={tour.image} alt={tour.name}/>
       <footer>
         <div>
-          
+          <h4>{tour.name}</h4>
+          <h4 className='tour-price'>${tour.price}</h4>
         </div>
+        <p className='tour-info'>
+          {readmore ? tour.info :`${(tour.info).substring(0,200) } . . .` }
+          <button onClick={()=>{
+            setreadmore(!readmore);
+            // this is alternative for this 
+            // if(readmore==false){
+            //   setreadmore(true)
+            // }else{
+            //   setreadmore(false)
+            // }
+          }}> {readmore ? 'show less' : 'show more'}</button>
+         
+        </p>
+        <button className='delete-btn'onClick={()=>{}} >not intersted</button>
       </footer>
 
   </article>
