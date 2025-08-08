475d2890526db09f383cc8f89bf9a2cf2d5089c9
hasset <hassetwendesen653@gmail.com>
2025-08-08 15:58:04 +0300

    incerase and decrease  function

--- review/src/Review.js
+++ review/src/Review.js
@@ -4,18 +4,27 @@ import { MdNavigateNext } from "react-icons/md";
 
 
 const Review = () => {
-  const [currentItem,setcurrentItem]=useState(0);
+  const [currentItem , setcurrentItem]=useState(0);
   const [person,setperson]=useState({})
   const showperson = (index) =>{
       setperson(people[index]);
       return console.log(person)
   }
+  const increase=()=>{
+    setcurrentItem((prev)=> prev< people.length-1?prev+1:0);
+     
+  }
+ const decrease=()=>{
+  setcurrentItem((prev)=>prev>0?prev-1:people.length-1)
+ }
+
   useEffect(
   ()=> {showperson(currentItem);},[currentItem]);
   return <section className='review'>
     <img src={person.image} alt='name' className='person-img'/>
     <h3>{person.name}</h3>
     <p>{person.text}</p>
+    <MdNavigateNext  onClick={()=>{increase()}}/>
 
   </section>;
 };
