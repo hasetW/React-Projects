f48872686dd2fdbc39610cc17e47c1b8526a9539
hasset <hassetwendesen653@gmail.com>
2025-08-08 22:18:49 +0300

    finshd the maping

--- according/src/App.js
+++ according/src/App.js
@@ -1,8 +1,17 @@
 import React, { useState } from 'react';
-import data from './data';
-import question from './question';
+import Question from './question';
+import questions from './data';
 function App() {
-  return <h2>accordion project setup</h2>;
+  return <section className='container'>
+    <h3>accordion project setup</h3>;
+    { questions.map((question)=>{
+      return <Question key={question.id} {...question}/>
+      
+    })}
+   
+
+  </section>
+  
 }
 
 export default App;
--- according/src/question.js
+++ according/src/question.js
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
-const question = () => {
-  return <h2>question component</h2>;
+const question = (question) => {
+    return <section className='question'>
+        <article>
+            <h4>{question.title}</h4>
+            <p>{question.info}</p>
+        </article>
+    </section>;
 };
 
 export default question;
