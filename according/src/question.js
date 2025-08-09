dbe8ad5f1a1b0f02db86d5aceee39e69db743d60
hasset <hassetwendesen653@gmail.com>
2025-08-09 17:00:35 +0300

    finshed the question

--- according/src/question.js
+++ according/src/question.js
@@ -1,16 +1,23 @@
 import React, { useState } from 'react';
-const question = (question) => {
+const Question = (question) => {
+    const [seeMore,setseeMore]=useState(false)
+    const toggleinfo=()=>{
+        setseeMore((prev)=>!prev)
+       
+    }
     return <section className='question'>
         <article>
-            <div>
+            <div className="inside">
                 <h4>{question.title}</h4>
-                <button className='btn'>+</button>
+                <button className='btn' onClick={toggleinfo}>{seeMore?" + ":" - "}</button>
             </div>
-            
-            
-            <p>{question.info}</p>
+            {seeMore && <p>{question.info}</p>}
         </article>
     </section>;
 };
 
-export default question;
+export default Question;
+// this is the same way as the bottome one
+// what we are saying is that if showinfo true render it
+// {seeMore && <p>{question.info}</p>}
+// {showInfo ? <p>{info}</p> : null}
