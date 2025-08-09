d78acf945afb31810a12fc5437691a48bc2d9d12
hasset <hassetwendesen653@gmail.com>
2025-08-09 17:38:58 +0300

    setting up the folders

--- menu/src/App.js
+++ menu/src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
 
 function App() {
-  
+  return <h2>menu project setup</h2>;
 }
-
 
 export default App;
