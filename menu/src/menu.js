1fb27713bd93529d20ebbaa1ad0386ac910c1e9a
hasset <hassetwendesen653@gmail.com>
2025-08-09 19:52:46 +0300

    commit

--- menu/src/menu.js
+++ menu/src/menu.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
 const Menu = (item) => {
-  return <section>
-    <h2>menu component</h2>;
-    <h3>{item.title}</h3>
-    <img src={item.img} alt='name'/>
+  return <section className='menu-item'>
+    <img className='photo' src={item.img} alt='name'/>
+    <div className='item-info'>
+         <header>
+            <h4>{item.title}</h4>
+            <p className='item-text'>{item.desc}</p>
+         </header>
+         
+    </div>
+   
+    
   </section>
    
 };
